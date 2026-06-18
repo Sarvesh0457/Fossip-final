a0d305614539648d18231269b77d8ea6c3612abe
Hackathon56 <anubhavsingh6268@gmail.com>
2026-06-18 16:39:57 +0530

    email verifi

--- Backend/src/utils/mail.js
+++ Backend/src/utils/mail.js
@@ -3,10 +3,13 @@ import nodemailer from "nodemailer";
 
 // Create transporter ONCE
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // Google App Password
+    pass: process.env.EMAIL_PASS,
   },
   connectionTimeout: 10000,
   greetingTimeout: 10000,
